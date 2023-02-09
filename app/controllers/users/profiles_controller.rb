c88b56fdc30ed089e10778ea9277571fac996d7b
nishimura017 <lo-2s.017@ezweb.ne.jp>
2023-02-09 20:08:48 +0900

    ユーザー登録機能まで。画像登録は未実装

---
+++ app/controllers/users/profiles_controller.rb
@@ -0,0 +1,34 @@
+class Users::ProfilesController < ApplicationController
+
+	class Users::ProfilesController < ApplicationController
+		before_action :set_user, only: [:show, :edit, :update]
+	
+		def show
+		end
+	
+		def edit
+		end
+	
+		def update
+			if @user.update(user_params)
+				flash[:notice] = "ユーザー情報を更新しました"
+				redirect_to user_path(@user)
+			else
+				flash.now[:alert] = "更新に失敗しました"
+				render :edit
+			end
+		end
+	
+		private
+	
+		def set_user
+			@user = current_user
+		end
+	
+		def user_params
+			params.require(:user).permit(:username, :profile)
+		end
+	end
+	
+	
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+ 
+  get 'users/profile/edit/', to: 'users/profiles#edit'
+  get 'users/profile/show/', to: 'users/profiles#show'
+  get '/users/:id', to: 'users/accounts#show'
+  patch 'users/:id', to: 'users/profiles#update'
+  root to: "home#index"
+  resources :users  
+
 end
+
